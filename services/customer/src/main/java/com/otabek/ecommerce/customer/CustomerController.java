08eb0fa415c84043eb9d708289ab53df2a576f5c
Atabek04 <otabek.shadimatov@gmail.com>
2024-07-29 17:13:53 +0500

    implemented Customer service

--- services/customer/src/main/java/com/otabek/ecommerce/customer/CustomerController.java
+++ services/customer/src/main/java/com/otabek/ecommerce/customer/CustomerController.java
@@ -3,10 +3,9 @@ package com.otabek.ecommerce.customer;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/customers")
@@ -20,4 +19,43 @@ public class CustomerController {
     ) {
         return ResponseEntity.ok(service.createCustomer(request));
     }
+
+    @PutMapping
+    public ResponseEntity<Void> updateCustomer(
+            @RequestBody @Valid CustomerRequest request
+    ) {
+        service.updateCustomer(request);
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping
+    public ResponseEntity<List<CustomerResponse>> findAll() {
+        return ResponseEntity.ok(service.findAllCustomers());
+    }
+
+    @GetMapping("exists/{id}")
+    public ResponseEntity<Boolean> existsById(@PathVariable String id) {
+        return ResponseEntity.ok(service.existsById(id));
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<CustomerResponse> findById(@PathVariable String id) {
+        return ResponseEntity.ok(service.findById(id));
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity<Void> deleteById(@PathVariable String id) {
+        service.deleteById(id);
+        return ResponseEntity.ok().build();
+    }
 }
+
+
+
+
+
+
+
+
+
+
